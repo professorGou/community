89c6a76acf1b01eb9c546849fb148bb04e64d248
gjj0703 <550170358@qq.com>
2020-06-23 23:25:34 +0800

    add login n IndexController

--- src/main/java/com/itpro/community/controller/IndexController.java
+++ src/main/java/com/itpro/community/controller/IndexController.java
@@ -1,4 +1,14 @@
 package com.itpro.community.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
